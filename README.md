45c08c8c730351cad25d29fd4fba24ca5439138a
madison-craig <madisoncraig@protonmail.com>
2021-11-17 19:13:51 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,41 @@
+## Username-Checker
+A simple username checker for many popular sites.
+
+<p align="center">
+    <img src="https://github.com/madison-craig/blob/main/screenshotusernamechecker.png">
+</p>
+
+### Available websites to check:
+- Github
+- Hackerone
+- Instagram
+- Medium
+- Minecraft
+- Pastebin
+- Quora
+- Reddit
+- Soundcloud
+- Sourceforge
+- TikTok
+- TryHackMe
+- Twitch
+- Twitter
+- YouTube
+
+## Install
+To install and use Username-Checker, you can build locally:
+```
+git clone https://github.com/madison-craig/username-checker.git
+cd username-checker/
+python3 username-checker.py
+```
+## Requirements
+You will need to intall `yaml` if username-checker will not run.
+```
+$ pip3 install yaml
+```
+or
+```
+$ sudo pacman -S python-yaml
+```
+### More websites will be added soon!
---
+++ username-checker.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+import os
+import yaml
+import requests
+
+# colors
+W  = '\033[0m'  # white (default)
+P  = '\033[1;35m' # purple
+C  = '\033[1;36m' # cyan
+    
+
+def checker():
+    handle = input(C+' Type ['+P+'exit'+C+'] to close the program\n \n Enter a username to search:\n \n > '+W)
+    if handle == 'exit':
+        exit()
+    else:
+        with open('websites.yml') as f:
+            data = yaml.safe_load(f)
+            links = data['sites']
+
+        weblinks = ["{}{}".format(i,handle) for i in links]
+
+        for url in weblinks:
+            r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'})
+            if r.status_code == 200:
+                print(C+'Unavailable for: '+W+'' + url)
+            else:
+                print(P+'Available for:   '+W+'' + url)
+        main()
+
+
+
+def banner():
+    try:
+        os.system('cls')
+        raise ValueError('Error')
+    except Exception:
+        os.system('clear')
+    print (C+'')
+    print (' .--.--.-----.-----.----.-----.---.-.--------.-----. ')
+    print (' |  |  |__ --|  -__|   _|     |  _  |        |  -__| ')
+    print (' |_____|_____|_____|__| |__|__|___._|__|__|__|_____| ')
+    print ('                                                     ')                                                
+    print ('            __               __                      ')
+    print ('      .----|  |--.-----.----|  |--.-----.----.       ')    
+    print ('      |  __|     |  -__|  __|    <|  -__|   _|       ')   
+    print ('      |____|__|__|_____|____|__|__|_____|__|         ')     
+    print ('                                                     ')
+    print (C+'              '+C+' Created by: Madison')
+    print (P+'             GitHub: ['+C+'madison-craig'+P+']')
+    print ('')
+
+def main():
+    checker()
+
+banner()
+main()
